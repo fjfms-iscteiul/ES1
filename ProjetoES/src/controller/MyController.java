496e5a1b45354b76840067dc28bc228d5160c37b
Anna <Anna@172.17.10.78>
2019-12-06 02:14:25 +0000

    Tentei fazer o excel aparecer de outra maneira, aparece as linhas mas não o conteudo

--- ProjetoES/src/controller/MyController.java
+++ ProjetoES/src/controller/MyController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
@@ -11,9 +13,12 @@ import java.util.ResourceBundle;
 import java.util.Set;
 
 import javax.swing.JFileChooser;
+import javax.swing.JTable;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -63,18 +68,18 @@ public class MyController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-//		method_id.setCellValueFactory(new PropertyValueFactory<Integer, String>("method_id"));
-//		package_name.setCellValueFactory(new PropertyValueFactory<String, String>("package_name"));
-//		class_name.setCellValueFactory(new PropertyValueFactory<String, String>("class_name"));
-//		method_name.setCellValueFactory(new PropertyValueFactory<String, String>("method_name"));
-//		loc.setCellValueFactory(new PropertyValueFactory<String, String>("loc"));
-//		cyclo.setCellValueFactory(new PropertyValueFactory<String, String>("cyclo"));
-//		aftd.setCellValueFactory(new PropertyValueFactory<String, String>("aftd"));
-//		laa.setCellValueFactory(new PropertyValueFactory<String, String>("laa"));
-//		is_long_method.setCellValueFactory(new PropertyValueFactory<String,String>("is_long_method"));
-//		iplasma.setCellValueFactory(new PropertyValueFactory<String, String>("iplasma"));
-//		pmd.setCellValueFactory(new PropertyValueFactory<String, String>("pmd"));
-//		is_feature_envy.setCellValueFactory(new PropertyValueFactory<String, String>("is_feature_envy"));
+		//		method_id.setCellValueFactory(new PropertyValueFactory<Integer, String>("method_id"));
+		//		package_name.setCellValueFactory(new PropertyValueFactory<String, String>("package_name"));
+		//		class_name.setCellValueFactory(new PropertyValueFactory<String, String>("class_name"));
+		//		method_name.setCellValueFactory(new PropertyValueFactory<String, String>("method_name"));
+		//		loc.setCellValueFactory(new PropertyValueFactory<String, String>("loc"));
+		//		cyclo.setCellValueFactory(new PropertyValueFactory<String, String>("cyclo"));
+		//		aftd.setCellValueFactory(new PropertyValueFactory<String, String>("aftd"));
+		//		laa.setCellValueFactory(new PropertyValueFactory<String, String>("laa"));
+		//		is_long_method.setCellValueFactory(new PropertyValueFactory<String,String>("is_long_method"));
+		//		iplasma.setCellValueFactory(new PropertyValueFactory<String, String>("iplasma"));
+		//		pmd.setCellValueFactory(new PropertyValueFactory<String, String>("pmd"));
+		//		is_feature_envy.setCellValueFactory(new PropertyValueFactory<String, String>("is_feature_envy"));
 
 	}
 
@@ -82,60 +87,131 @@ public class MyController implements Initializable {
 	private Button button_import;
 
 	
+	//CODIGO CAROLINA
+	@FXML
+	TableView tabela;
+
 	/* Chooses file and inputs the path to readExcel */
 	public void import_file(ActionEvent event) {
-		try {
-			System.out.println("importa");
-			String defaultCurrentDirectoryPath = "C:\\users\\Authentic\\Desktop";
-			JFileChooser excelFile = new JFileChooser(defaultCurrentDirectoryPath);
-			excelFile.showOpenDialog(null);
-			readExcel(excelFile.getSelectedFile().getAbsolutePath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 
-	/* Reads the ExcelFile and stores it in a Hash Map */
-	private void readExcel(String excel_file) throws IOException {
+		File excelFile;
+		FileInputStream excelFIS = null;
+		BufferedInputStream excelBIS = null;
+		XSSFWorkbook excelJTableImport;
 
-		File excelFile = new File(excel_file);
-		FileInputStream fis = new FileInputStream(excelFile);
+		System.out.println("importa");
 
-		workbook = new XSSFWorkbook(fis);
-		XSSFSheet sheet = workbook.getSheetAt(0);
+		String defaultCurrentDirectoryPath = "C:\\users\\Authentic\\Desktop";
+		JFileChooser excelFileChooser = new JFileChooser(defaultCurrentDirectoryPath);
+		int excelChooser = excelFileChooser.showOpenDialog(null);
 
-		excel_table = new HashMap<Integer, String>();
+		//	readExcel(excelFileChooser.getSelectedFile().getAbsolutePath());
 
-		/* Iterate in rows */
-		Iterator<Row> rowIt = sheet.iterator();
+		if(excelChooser == JFileChooser.APPROVE_OPTION) {
 
-		while (rowIt.hasNext()) {
-			Row row = rowIt.next();
+			try {
+				excelFile = excelFileChooser.getSelectedFile();
+				excelFIS = new FileInputStream(excelFile);
+				excelBIS = new BufferedInputStream(excelFIS);
 
-			/* Iterate on cells for the current row */
-			Iterator<Cell> cellIterator = row.cellIterator();
 
-			int inicial =1;
-			while (cellIterator.hasNext()) {
-				Cell cell = cellIterator.next();
-				excel_table.put(inicial, cell.toString());
-				inicial++;
-//				System.out.print(cell.toString() + ";");
+				excelJTableImport = new XSSFWorkbook(excelBIS);
+				XSSFSheet excelSheet = excelJTableImport.getSheetAt(0);
+
+				for (int i = 1; i < excelSheet.getLastRowNum(); i++) {
+					XSSFRow excelRow = excelSheet.getRow(i);
+
+
+					XSSFCell excelMethodID = excelRow.getCell(0);
+					XSSFCell excelPackae = excelRow.getCell(1);
+					XSSFCell excelClass = excelRow.getCell(2);
+					XSSFCell excelMethod = excelRow.getCell(3);
+					XSSFCell excelLOC = excelRow.getCell(4);
+					XSSFCell excelCYCLO = excelRow.getCell(5);
+					XSSFCell excelATFD = excelRow.getCell(6);
+					XSSFCell excelLAA = excelRow.getCell(7);
+					XSSFCell excelIs_long_method = excelRow.getCell(8);
+					XSSFCell excelIPlasma = excelRow.getCell(9);
+					XSSFCell excelPMD = excelRow.getCell(10);
+					XSSFCell excelIs_feature_envy = excelRow.getCell(11);
+
+					System.out.println(excelMethodID);
+					System.out.println(excelPackae);
+					System.out.println(excelClass);
+					System.out.println(excelMethod);
+
+
+					tabela.getItems().addAll(excelMethodID, excelPackae, excelClass, excelMethod,
+							excelLOC,excelCYCLO,excelATFD,excelLAA,excelIs_long_method,excelIPlasma,
+							excelPMD,excelIs_feature_envy);
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-				// Coordinate co = new Coordinate(x, y);
-
-				// excel_table.put(co, cell.toString());
-
-				// x++;
-
-			System.out.println(excel_table.toString());
-						// y++;
-
 		}
 
-		workbook.close();
-		fis.close();
 
 	}
+
+
+	//CODIGO DO BRAGANÇA
+	//	/* Chooses file and inputs the path to readExcel */
+	//	public void import_file(ActionEvent event) {
+	//		try {
+	//			System.out.println("importa");
+	//			
+	//			
+	//			String defaultCurrentDirectoryPath = "C:\\users\\Authentic\\Desktop";
+	//			JFileChooser excelFile = new JFileChooser(defaultCurrentDirectoryPath);
+	//			excelFile.showOpenDialog(null);
+	//			readExcel(excelFile.getSelectedFile().getAbsolutePath());
+	//		} catch (IOException e) {
+	//			e.printStackTrace();
+	//		}
+	//	}
+	//	/* Reads the ExcelFile and stores it in a Hash Map */
+	//	private void readExcel(String excel_file) throws IOException {
+	//
+	//		File excelFile = new File(excel_file);
+	//		FileInputStream fis = new FileInputStream(excelFile);
+	//
+	//		workbook = new XSSFWorkbook(fis);
+	//		XSSFSheet sheet = workbook.getSheetAt(0);
+	//
+	//		excel_table = new HashMap<Integer, String>();
+	//
+	//		/* Iterate in rows */
+	//		Iterator<Row> rowIt = sheet.iterator();
+	//
+	//		while (rowIt.hasNext()) {
+	//			Row row = rowIt.next();
+	//
+	//			/* Iterate on cells for the current row */
+	//			Iterator<Cell> cellIterator = row.cellIterator();
+	//
+	//			int inicial =1;
+	//			while (cellIterator.hasNext()) {
+	//				Cell cell = cellIterator.next();
+	//				excel_table.put(inicial, cell.toString());
+	//				inicial++;
+	////				System.out.print(cell.toString() + ";");
+	//			}
+	//				// Coordinate co = new Coordinate(x, y);
+	//
+	//				// excel_table.put(co, cell.toString());
+	//
+	//				// x++;
+	//
+	//			System.out.println(excel_table.toString());
+	//						// y++;
+	//
+	//		}
+	//
+	//		workbook.close();
+	//		fis.close();
+	//
+	//	}
 
 }
