2223a9da02673a55a42b4fb5dcd1cdbf02b0bbd8
hncos-iscteiul <hncos@iscte-iul.pt>
2019-12-11 20:50:30 +0000

    transferir para o mew branch

--- ProjetoES/src/functional/MyController.java
+++ ProjetoES/src/functional/MyController.java
@@ -15,17 +15,22 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import javafx.fxml.Initializable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class MyController implements Initializable {
 
@@ -84,6 +89,19 @@ public class MyController implements Initializable {
 		}
 
 	}
+	
+	@FXML
+	private Button Thresholds;
+	
+	
+	@FXML
+	public void Thresholds(ActionEvent event) throws Exception {
+		Parent border = FXMLLoader.load(getClass().getResource("paneAUX.fxml"));
+		Scene borderTH = new Scene(border);
+		Stage novoPanel = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		novoPanel.setScene(borderTH);
+		novoPanel.show();
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -135,7 +153,6 @@ public class MyController implements Initializable {
 				}
 				if (cell.getColumnIndex() == 1) {
 					tableRow.setPackageName(cell.toString());
-					System.out.println(cell.toString());
 
 				}
 				if (cell.getColumnIndex() == 2) {
